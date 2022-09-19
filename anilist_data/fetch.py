e943716c11ad195bf61ccf0ed6c0f75f499c21e3
barrettotte <barrettotte@gmail.com>
2022-09-18 20:22:22 -0400

    fetch user data

---
+++ anilist_data/fetch.py
@@ -0,0 +1,50 @@
+'''
+Utilities for fetching data from Anilist via their GraphQL API.
+'''
+import os
+import re
+import requests
+
+ANILIST_API = 'https://graphql.anilist.co'
+TIMEOUT_SECS = 30
+PER_PAGE = 50
+
+def gql_src(gql_path: str) -> str:
+    '''
+    Opens GraphQL file source
+    '''
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(this_dir, gql_path), encoding='utf-8') as gql_file:
+        return re.sub(r'\s+', ' ', ''.join(gql_file.readlines()))
+
+def anilist_req(gql: str, variables: dict):
+    '''
+    Generic Anilist GraphQL request with query and variables.
+    '''
+    headers = {'Content-Type': 'application/json'}
+    body = {
+        'query': gql,
+        'variables': variables
+    }
+    resp = requests.post(ANILIST_API, headers=headers, json=body, timeout=TIMEOUT_SECS)
+    if resp.status_code != 200:
+        raise Exception('Failed to request Anilist data.\nRequest body: '
+            f'{body}\n{resp.status_code} {resp.content}')
+    return resp.json()
+
+def get_user(user_id: int) -> dict:
+    '''
+    Fetches Anilist user data
+    '''
+    return anilist_req(gql_src('gql/user.gql'), {'id': user_id})
+
+# fetch anime data
+def get_anime(anime_id: int, out_csv: str) -> dict:
+    '''
+    Fetches single Anilist anime entry
+    '''
+
+def download_anime_range(start: int, end: int, out_csv: str) -> int:
+    '''
+    Downloads all Anilist anime data in date range (YYYYMMDD-YYYYMMDD) to CSV; Returns entry count
+    '''
--- data/anilist.py
+++
@@ -1,2 +0,0 @@
-# Download Anilist anime data to a CSV via their GraphQL API.
-# If script stopped, it will pick up where it left off
